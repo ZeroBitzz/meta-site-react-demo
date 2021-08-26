ed4c34e5fc8209c37087227793dac36b84e63b5a
zero <zerothesu@gmail.com>
2021-08-26 16:34:04 -0600

    finalized app, removed arbitary files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
 
 function App() {
+  //grabs current date and time
+  const currentTime = Date().toLocaleString()
+  //hook for time so you can render with every update to time
+  const [time, setTime] = useState(currentTime);
+
+  //x and y position hooks for the mouse when hovering on the site
+  const [posX, setPosX] = useState(0);
+  const [posY, setPosY] = useState(0);
+
+  //useEffect to update time after every second (1000 milliseconds)
+  useEffect(() => {
+    const intervalId = setInterval(() => {setTime((prev) => prev = Date().toLocaleString())}, 1000)
+    return () => {
+      clearInterval(intervalId);
+    };
+    //uses a blank dependency array so that useEffect renders once and stays running uninterupted
+  }, [])
+
+  //function to update the x and y values on the page every time you move the mouse
+  const onMouseMove = (e) => {
+      setPosX((prev) => prev = e.clientX);
+      setPosY((prev) => prev = e.clientY);
+  }
+
+  //return statement
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className="App" onMouseMove={onMouseMove}>
+        <h1>" meta site"</h1>
+        <h3>"{time}"</h3>
+        <p >X:{posX}</p>
+        <p>Y:{posY}</p>
+      </div>
     </div>
+    
   );
 }
 
